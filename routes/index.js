4db1791af19600e8d76ab14b2aa3abbc02c8370d
Jacob Stuart <stuart4@purdue.edu>
2016-04-19 20:54:26 -0400

    welcome to the next generation

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'StorkStalker' });
+  res.render('index', { title: 'StorkStalker-NG' });
 });
 
 module.exports = router;
