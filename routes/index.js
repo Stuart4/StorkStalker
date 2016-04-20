5eecaf40afc61966116077f189167b35755567e7
Jacob Stuart <stuart4@purdue.edu>
2016-04-19 21:24:03 -0400

    removes the stupid index.html and renames stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'StorkStalker-NG' });
+  res.render('index', { title: 'StorkStalker\'s Index' });
 });
 
 module.exports = router;
