68f0d3ac0c445746f9c18ec9cd96c2b8d82c7ac0
Jacob Stuart <stuart4@purdue.edu>
2016-04-19 23:22:11 -0400

    completes post creation of tracking at /tracking with uid and tracking_code

---
+++ routes/tracking.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var easypost = require('node-easypost')(process.env.easypost_apikey_production);
+mongoose.connect(process.env.mlabs_uri_production);
+
+/* POST new tracking with uid and tracking_code. */
+router.post('/', function(req, res) {
+    var trackingLocationSchema = mongoose.Schema({
+        city: String,
+        state: String,
+        country: String,
+        zip: String
+    });
+    var trackingDetailsSchema = mongoose.Schema({
+        message: String,
+        status: String,
+        datetime: String,
+        source: String,
+        tracking_location: trackingLocationSchema
+    });
+    var trackingSchema = mongoose.Schema({
+        cls: String,
+        apiKey: String,
+        id: String,
+        object: String,
+        mode: String,
+        tracking_code: String,
+        status: String,
+        created_at: String,
+        updated_at: String,
+        signed_by: String,
+        weight: Number,
+        est_delivery_date: String,
+        shipment_id: String,
+        carrier: String,
+        tracking_details: [trackingDetailsSchema],
+        uid: Number
+    });
+    var TrackingModel = mongoose.model('TrackingModel', trackingSchema);
+
+    easypost.Tracker.create({ 'tracking_code': req.body.tracking_code}, function(err,res) {
+        res['uid'] = req.body.uid;
+        var child = new TrackingModel(res);
+
+        child.save(function (err) {
+            if (err) throw err;
+            console.log('added a new document to the mongo')
+
+        });
+
+    });
+    res.send('success');
+    });
+
+
+module.exports = router;
